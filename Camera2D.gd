f9a35c429c7227241c9fa05e2af0edd24f83da1a
mikelee53 <58196525+mikelee53@users.noreply.github.com>
2022-01-28 23:43:27 -0800

    first commit
    added BG, player, boss sprites with basic idle anims + screen shake

---
+++ Camera2D.gd
@@ -0,0 +1,35 @@
+extends Camera2D
+
+onready var shake_timer = get_node("Shake Timer")
+
+var current_state : String
+export var shake_strength = 6
+
+func _ready():
+	register_signals()
+
+func shake_camera():
+	offset.x = rand_range(-1.0, 1.0) * shake_strength
+	offset.y = rand_range(-1.0, 1.0) * shake_strength
+
+func update_state(new_state):
+	current_state = new_state
+	match new_state:
+		"SHAKE":
+			shake_timer.start(0.2)
+
+func register_signals():
+	EventBus.connect("camera_event_initiated", self, "update_state")
+
+#debugging purposes
+func _input(event):
+	if Input.is_action_just_pressed("ui_down"):
+		EventBus.emit_signal("camera_event_initiated", "SHAKE")
+
+func _process(delta):
+	if shake_timer and !shake_timer.is_stopped():
+		shake_camera()
+		print(offset)
+
+func _on_Shake_Timer_timeout():
+	offset = Vector2(0,0)
---
+++ Events/EventBus.gd
@@ -0,0 +1,4 @@
+extends Node
+
+signal player_hit(dmg_amt)
+signal camera_event_initiated(type)
